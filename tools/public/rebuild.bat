94ad289987c2c034e8df50369a29e76174bacb57
NingLeiXueR <348634371@qq.com>
2023-11-13 15:15:08 +0800

    简单修改
    简单修改

--- tools/public/rebuild.bat
+++ tools/public/rebuild.bat
@@ -1,4 +1,4 @@
-﻿xcopy ..\..\bin\Debug\rebuild.exe .\ /s/y
+xcopy ..\..\bin\Debug\rebuild.exe .\ /s/y
 rebuild.exe ../../server/libmycorekkk/core/ub.cpp ../../public 
 cd ../../bin/Debug
 makeproto.exe
