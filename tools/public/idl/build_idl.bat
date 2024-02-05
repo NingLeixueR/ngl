78025922d112863a1cf885fb75e89630c1308bd1
NingLeiXueR <348634371@qq.com>
2024-02-05 17:13:50 +0800

    包头增加版本号
    包头增加版本号

--- tools/public/idl/build_idl.bat
+++ tools/public/idl/build_idl.bat
@@ -3,5 +3,5 @@ xcopy ..\..\..\bin\Release\IDL.exe .\ /s/y
 IDL.exe
 xcopy idlfile\*.h ..\..\..\public\cpp\protocol\ /s/y
 xcopy idlfile\*.cpp ..\..\..\public\cpp\protocol\ /s/y
-xcopy idlfile\*.cs ..\..\..\public\csharp\csv\ /s/y
+xcopy idlfile\*.cs ..\..\..\public\csharp\csharp\csharp\csv\ /s/y
 pause
