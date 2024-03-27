c7b8c97667c488805b7774861bb8a4bf8b8a6b49
NingLeiXueR <348634371@qq.com>
2024-03-27 20:22:44 +0800

    去掉gm.pro

--- tools/public/proto/build_protocol.bat
+++ tools/public/proto/build_protocol.bat
@@ -1,8 +1,5 @@
-protoc.exe --experimental_allow_proto3_optional  --cpp_out=./pro data.proto
 protoc.exe --experimental_allow_proto3_optional  --cpp_out=./pro net.proto
 protoc.exe --experimental_allow_proto3_optional  --cpp_out=./pro db.proto
-protoc.exe --experimental_allow_proto3_optional  --cpp_out=./pro gm.proto
-protoc.exe --experimental_allow_proto3_optional --php_out=./pro/phpcode/gm gm.proto
 protoc.exe --experimental_allow_proto3_optional --php_out=./pro/phpcode/db db.proto
 
 protoc.exe --experimental_allow_proto3_optional  --csharp_out=./pro net.proto
@@ -13,9 +10,6 @@ xcopy pro\*.h ..\..\..\public\cpp\protocol\protobuf /s/y
 xcopy pro\*.cs ..\..\..\public\csharp\csharp\csharp\protobuf /s/y
 
 xcopy /E /I /Y pro\phpcode  ..\..\..\php\
-#xcopy pro\Gm\*.php ..\..\php\phpcode\protobuf\Gm /s/y
-#xcopy pro\GPBMetadata\*.php ..\..\php\phpcode\protobuf\GPBMetadata /s/y
-
 
 xcopy ..\..\..\bin\Debug\makeproto.exe .\ /s/y
 xcopy ..\..\..\bin\Release\makeproto.exe .\ /s/y
