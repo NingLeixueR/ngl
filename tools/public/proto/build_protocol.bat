2a36c9127a13b452841f3fe2e4e56d11b3aab653
NingLeiXueR <348634371@qq.com>
2024-04-15 15:13:38 +0800

    任务系统优化

--- tools/public/proto/build_protocol.bat
+++ tools/public/proto/build_protocol.bat
@@ -1,6 +1,6 @@
 protoc.exe --experimental_allow_proto3_optional  --cpp_out=./pro net.proto
 protoc.exe --experimental_allow_proto3_optional  --cpp_out=./pro db.proto
-protoc.exe --experimental_allow_proto3_optional --php_out=./pro/phpcode/db db.proto
+#protoc.exe --experimental_allow_proto3_optional --php_out=./pro/phpcode/db db.proto
 
 protoc.exe --experimental_allow_proto3_optional  --csharp_out=./pro net.proto
 protoc.exe --experimental_allow_proto3_optional  --csharp_out=./pro db.proto
@@ -9,7 +9,7 @@ xcopy pro\*.cc ..\..\..\public\cpp\protocol\protobuf /s/y
 xcopy pro\*.h ..\..\..\public\cpp\protocol\protobuf /s/y
 xcopy pro\*.cs ..\..\..\public\csharp\csharp\csharp\protobuf /s/y
 
-xcopy /E /I /Y pro\phpcode  ..\..\..\php\
+#xcopy /E /I /Y pro\phpcode  ..\..\..\php\
 
 xcopy ..\..\..\bin\Debug\makeproto.exe .\ /s/y
 xcopy ..\..\..\bin\Release\makeproto.exe .\ /s/y
