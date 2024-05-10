60439c2ec122d25d506b981026cb8473c2357441
NingLeiXueR <348634371@qq.com>
2024-05-10 10:56:22 +0800

    删除无用文件

--- tools/public/rebuild.bat
+++
@@ -1,5 +0,0 @@
-xcopy ..\..\bin\Debug\rebuild.exe .\ /s/y
-xcopy ..\..\bin\Release\rebuild.exe .\ /s/y
-#rebuild.exe ../../server/libmycorekkk/core/ub.cpp ../../public/cpp 
-#rebuild.exe ../../server/node/core/ub.cpp ../../public/cpp 
-#rebuild.exe ../../server/makeproto/core/ub.cpp ../../public/cpp 
